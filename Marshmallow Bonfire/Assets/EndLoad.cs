72e052e95a6113862061b0d2632b9477c51e8edd
Ruthenium <rutaspk@gmail.com>
2019-06-04 03:56:47 +0200

    End scene

---
+++ Marshmallow Bonfire/Assets/EndLoad.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndLoad : MonoBehaviour
+{
+    SceneScripts scriptsScene;
+
+    void Start()
+    {
+        scriptsScene = GetComponent<SceneScripts>();
+        StartCoroutine(Load());
+    }
+
+    IEnumerator Load()
+    {
+        yield return new WaitForSeconds(6);
+        scriptsScene.LoadLevel(1);
+    }
+}
